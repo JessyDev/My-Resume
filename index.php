b62dffa4500b50899318c89ddd6ce83f9a870f09
JessyDev <chetsada001@gmail.com>
2015-05-05 01:05:10 +0700

    test3

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">	
-	<title>Bootstrap tutorials by Siam HTML</title>
+	<title>Bootstrap tutorials by Siam HTMLsss</title>
 </head>
 <body>
 123456
@@ -14,11 +14,12 @@
 			<div class="span8">MAIN</div>
 				<div class="span2">Menu</div>
 				<div class="span6">Content</div>
-			<div class="span4 offset6">SIDE</div>
+			<div class="span4 offset6">SIDEtest</div>
 		</div>
 	</div>
-
+aaaa
 	<script src="js/jquery-1.11.2.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	Foot
 </body>
 </html>
