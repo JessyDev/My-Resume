0ba49c9f8961b34147b95be3e52b1db48a658098
JessyDev <chetsada001@gmail.com>
2015-05-05 01:01:38 +0700

    Test Git

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">	
+	<title>Bootstrap tutorials by Siam HTML</title>
+</head>
+<body>
+
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span8">MAIN</div>
+				<div class="span2">Menu</div>
+				<div class="span6">Content</div>
+			<div class="span4 offset6">SIDE</div>
+		</div>
+	</div>
+
+	<script src="js/jquery-1.11.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
